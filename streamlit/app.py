4847679671a0204a1b28940320882e24c19b425f
xudaxiansheng <1610749979@qq.com>
2024-08-26 17:57:54 +0800

    Initial commit

---
+++ streamlit/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import time
+
+# 登录页面练习
+
+def login(name, psw):
+    time.sleep(2)
+    return name == 'xu' and psw == '123'
+
+
+name = st.text_input('name', 'xu')
+psw = st.text_input('password', '123')
+
+button = st.button('login')
+if button:
+    with st.spinner('loading....'):
+        rel = login(name, psw)
+        if rel:
+            st.write('登录成功')
+        else:
+            st.write('失败')

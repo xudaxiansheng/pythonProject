2cf3a9b6e6736a59eed50b5c846f83729e4914f8
xudaxiansheng <1610749979@qq.com>
2024-09-07 23:31:35 +0800

    Merge remote-tracking branch 'origin/master'

--- streamlit/pages/2_bmi.py
+++ streamlit/pages/2_bmi.py
@@ -1,4 +1,8 @@
+import json
+
 import streamlit as st
+import pandas as pd
+import time
 
 # BMI计算
 st.write('### BMI v1.0')
@@ -8,22 +12,58 @@ weight = float(
 height /= 100
 BMI = round(weight / (height ** 2), 2)
 button = st.button('BMI计算')
+st.caption('BMI计算公式：')  # 小号字体
+st.code('weight / (height ** 2) # 体重(kg)/身高(米)²', 'python')
 if button:
-    st.write(f'您的BMI是{BMI}')
+    st.caption(f'您的BMI是{BMI}')
     if BMI <= 18.40:
-        st.write('偏瘦')
+        st.write('偏瘦，需要增肥了！')
     elif BMI > 18.40 and BMI < 24:
-        st.write('正常')
+        st.write('正常，继续保持！')
     elif BMI >= 24 and BMI < 28:
-        st.write('过重')
+        st.write('过重，该减肥了！')
     else:
-        st.write('超重')
-
-    import pandas as pd
+        st.write('超重，太该减肥了！')
+    st.caption('BMI范围表如下')
 
     data = {
         '分类': ['偏瘦', '正常', '过重', '超重'],
         'BMI范围': ['<= 18.40', '18.40~23.99', '24~27.99', '>=28']
     }
     df = pd.DataFrame(data)
-    st.dataframe(df)
+    st.dataframe(df, width=1000, hide_index=True)
+    # 获取当前日期
+    current_date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    # 需要在文件中保存日期/身高/体重/BMI/分类
+    # dict_data = dict()
+    with open('BMI.txt', 'a') as f:
+        f.write(f'{current_date},{height},{weight},{BMI} \n')
+    # 日期列表
+    data_date = []
+    # 身高列表
+    data_height = []
+    # 体重列表
+    data_weight = []
+    # BMI列表
+    data_BMI = []
+    # 分类列表
+    # data_classification = []
+    with open('BMI.txt', 'r') as f:
+        content = f.readlines()
+        for i in content:
+            # st.write(f'{i}')
+            list_content = i.split(',')
+            data_date.append(list_content[0])
+            data_height.append(float(list_content[1]))
+            data_weight.append(float(list_content[2]))
+            data_BMI.append(float(list_content[3]))
+    data_cla = {
+        '日期': data_date,
+        '身高(m)': data_height,
+        '体重(kg)': data_weight,
+        'BMI': data_BMI,
+        # 'classification': data_classification
+    }
+    df1 = pd.DataFrame(data_cla)
+    st.caption('BMI数据记录表')  # 小号字体
+    st.dataframe(df1, width=1000, hide_index=True)
